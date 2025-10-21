b9c59c0cc2505a55b67af7775dcdcf4532867afa
Ali <Ali.1234@example.com>
2025-10-21 18:40:29 +0330

    added making a calculator in html and js

--- index/javascript.js
+++ index/javascript.js
@@ -1,1 +1,44 @@
+let firstNumber = "";
+let secondNumber = "";
+let operation = null;
 
+function appendNumber(num) {
+  let display = document.getElementById("display");
+  if (!operation) {
+    firstNumber += num;
+    display.innerHTML = firstNumber;
+  } else {
+    secondNumber += num;
+    display.innerHTML = secondNumber;
+  }
+}
+function setOperation(op) {
+  if (firstNumber == "") return;
+  operation = op;
+  document.getElementById("display").innerHTML = operation;
+}
+function calculate() {
+  if (firstNumber == "" || secondNumber == "" || !operation) return;
+  let result = 0;
+  let num1 = parseFloat(firstNumber);
+  let num2 = parseFloat(secondNumber);
+  if (operation == "+") {
+    result = num1 + num2;
+  } else if (operation == "-") {
+    result = num1 - num2;
+  } else if (operation == "/") {
+    result = num1 / num2;
+  } else if (operation == "*") {
+    result = num1 * num2;
+  }
+  document.getElementById("display").innerHTML = result;
+  firstNumber = result.toString();
+  secondNumber = "";
+  operation = null;
+}
+function clearDisplay() {
+  firstNumber = "";
+  secondNumber = "";
+  operation = null;
+  document.getElementById("display").innerHTML = 0;
+}
